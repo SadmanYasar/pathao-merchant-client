5f10550458025b0177d3c4ab2e29ed44a1d2c6c0
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-03-28 02:02:56 +0600

    json converts to csv

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,32 @@ import json2csv from 'json2csv';
 
 const App = () => {
   const [name, setname] = useState('')
+  const [phone, setphone] = useState('')
+  //const [data, setdata] = useState([])
+  const fields = ['Name', 'Phone']
+  const options = { fields }
 
   const handleSubmit = (e: any) => {
     e.preventDefault()
-    console.log('ok')
+    const arr = [
+      {
+        'Name': name,
+        'Phone': phone
+      },
+      {
+        'Name': 'john',
+        'Phone': '12354'
+      },
+      {
+        'Name': 'elton',
+        'Phone': '222333'
+      },
+    ]
+
+    json2csv
+      .parseAsync(arr, options)
+      .then(csv => console.log(csv))
+      .catch(e => console.log(e))
   }
 
   return (
@@ -19,6 +41,14 @@ const App = () => {
             id='name' 
             value={name}
             onChange={({target}) => setname(target.value)}/>
+        </label>
+        <label>
+          Phone:
+          <input 
+            type='text' 
+            id='phone' 
+            value={phone}
+            onChange={({target}) => setphone(target.value)}/>
         </label>
         <button type='submit'>Add</button>
       </form>
