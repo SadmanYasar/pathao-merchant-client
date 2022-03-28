eb131f5b828da8243bc8c3791181b4c083e7eb01
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-03-29 02:39:48 +0600

    Added order list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import OrderList from './components/OrderList';
 import ProductForm from './components/ProductForm';
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
   return (
     <div className="App">
       <ProductForm />
+      <OrderList />
     </div>
   );
 }
