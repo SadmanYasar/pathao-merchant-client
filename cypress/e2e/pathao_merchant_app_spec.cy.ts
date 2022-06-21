4468982092374a962a935647e992612a13808dd0
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-06-21 21:14:48 +0600

    added cypress

---
+++ cypress/e2e/pathao_merchant_app_spec.cy.ts
@@ -0,0 +1,9 @@
+describe('Pathao Merchant App', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function() {
+    cy.contains('Shipments')
+  })
+})
