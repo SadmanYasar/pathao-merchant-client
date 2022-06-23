75620965369fc5bff247804d8f2942f60b987be9
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-06-23 20:24:44 +0600

    update favicon, update cypress test folder

--- cypress/integration/pathao_merchant_app.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('Pathao Merchant App', function() {
-    beforeEach(function() {
-      cy.visit('http://localhost:3000')
-    })
-  
-    it('front page can be opened', function() {
-      cy.contains('Shipments')
-    })
-  })
